0f93178fb21253f30621ee8b8d81451abec6bcf7
Dan Johnson <danimal.johnson@gmail.com>
2020-07-28 07:55:45 -0700

    chore: adds click handlers, bomb display, and empty space clearing

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
   let height=10;
   let bombCount = 20;
   let squares = [];
+  let isGameOver = false;
 
   function createBoard() {
     const bombsArray = Array(bombCount).fill('bomb');
     const emptyArray = Array((width*height) - bombCount).fill('valid');
     const gameArray = emptyArray.concat(bombsArray);
+    
     // TODO: Improve randomness
     const shuffledArray = gameArray.sort(() => Math.random() -0.5);
 
@@ -20,9 +22,12 @@ document.addEventListener('DOMContentLoaded', () => {
       const square = document.createElement('div');
       square.setAttribute('id', i);
       square.classList.add(shuffledArray[i])
-      square.innerHTML = i; // TODO: Replace square number with bomb #
+      // square.innerHTML = i; // Replace square number with bomb #
       grid.appendChild(square);
       squares.push(square);
+
+      // Left click
+      square.addEventListener("click", ()=>{leftClickHandler(square)})
     }
 
     // Add bomb proximity numbers to each square.
@@ -32,8 +37,6 @@ document.addEventListener('DOMContentLoaded', () => {
       const isRightEdge = i % width === width-1;
       const isTopEdge = i < width;
       const isBottomEdge = (i > (squares.length - width - 1));
-      // console.log(`${i}: squares.length=${squares.length}, width=${width}, isBottomEdge= ${isBottomEdge}`);
-      // if (isBottomEdge) console.log(`Bottom (${i})`);
 
       if (squares[i].classList.contains('valid')) {
         // Look left
@@ -60,14 +63,83 @@ document.addEventListener('DOMContentLoaded', () => {
         // Lower right
         if (!isBottomEdge && !isRightEdge && squares[i+width+1].classList.contains('bomb'))
           nearbyBombs++;
-        
-        document.getElementById(i).innerHTML = nearbyBombs;
+        squares[i].setAttribute('data', nearbyBombs);
       }
     }
+  }
 
+  createBoard();
 
+  // Left click
+  function leftClickHandler(square) {
+    let currentId = square.id;
+    if (isGameOver)
+      return;
+    if (square.classList.contains('checked') || square.classList.contains('flag'))
+      return;
+    if (square.classList.contains('bomb')) {
+      square.innerHTML=('B');
+      isGameOver = true;
+    } else {
+      const bombs = square.getAttribute('data');
+      if(bombs != 0) {
+        square.innerHTML = square.getAttribute('data');
+      } else {
+        square.classList.add('clear');
+        checkSquare(square, currentId);
+      }
+      square.classList.add('checked');
+    }
+  }
+
+  function checkSquare(square, currentId) {
+    const isLeftEdge = currentId % width === 0;
+    const isRightEdge = (currentId % width === width - 1);
+    const isTopEdge = currentId < width;
+    const isBottomEdge = (currentId > (squares.length - width - 1));
+
+    // Should this just use a promise instead?
+    setTimeout(() => {
+
+      // Look left OK
+      if(currentId > 0 && !isLeftEdge) {
+        const newId = squares[currentId - 1].id;
+        const newSquare = document.getElementById(newId);
+        leftClickHandler(newSquare);
+      }
+
+      // Upper diagonal?
+      // if(!isTopEdge && !isRightEdge) {
+      //   const newId = squares[parseInt(currentId) + 1 - width].id;
+      //   const newSquare = document.getElementById(newId);
+      //   leftClickHandler(newSquare);
+      // }
+
+      // Look up OK
+      if(!isTopEdge) {
+        const newId = squares[currentId - width].id;
+        const newSquare = document.getElementById(newId);
+        leftClickHandler(newSquare);
+      }
+
+      // Look Right OK
+      if(currentId < squares.length-1 && !isRightEdge) {
+        const newId = squares[parseInt(currentId)+1].id;
+        const newSquare = document.getElementById(newId);
+        leftClickHandler(newSquare);
+      }
+
+      // Look down OK
+      if(!isBottomEdge) {
+        console.log("Current Id:", currentId);
+        const newId = squares[parseInt(currentId)+width].id;
+        console.log("New Id:",newId);
+        const newSquare = document.getElementById(newId);
+        leftClickHandler(newSquare);
+      }
+
+    }, 10);
 
   }
-  createBoard();
 
 });
