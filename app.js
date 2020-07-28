b0e51b7320c6cf9032855d7275375708218bed6d
Dan Johnson <danimal.johnson@gmail.com>
2020-07-27 18:04:05 -0700

    chore: Add createBoard function

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("sth");
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  let width=10;
+  let height=10;
+  let squares = [];
+
+
+  function createBoard() {
+    for (let i=0; i < width*height; i++) {
+      const square = document.createElement('div');
+      square.setAttribute('id', i);
+      grid.appendChild(square);
+      squares.push(square);
+    }
+  }
+  createBoard();
+
+});
